6f49d747d9bd96950093862eece44f5ffdcaf046
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-28 20:14:07 +0100

    saving login data inside store

--- Login.js
+++ Login.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { login } from './redux/actions'
-import { View } from 'react-native'
+import { View, TextInput } from 'react-native'
 import { Input, Button } from 'react-native-elements'
 
 class Login extends Component {
   constructor(props){
     super(props)
-    this.state = {input: ''}
+    this.state = {user: {email: '', password: ''}}
   }
 
   login = () => {
-    this.props.login(this.state.input)
-    this.setState({ input: '' })
+    this.props.login(this.state.user)
+    this.setState({ user: {email: '', password: ''}})
+  }
+
+  changeEmail(email){
+    new_state = {email: email, password: this.state.user.password}
+    this.setState({user: new_state}) 
+  }
+
+  changePassword(password){
+    new_state = {email: this.state.user.email, password: password}
+    this.setState({user: new_state})
   }
 
   render() {
     return(
       <React.Fragment>
         <View>
-          <Input onChangeText={(text) => this.setState({input: text})} value={this.state.input} />
+          <Input
+            onChangeText={(email) => this.changeEmail(email)}
+            value={this.state.user.email} />
+          <Input
+            onChangeText={(password) => this.changePassword(password)}
+            value={this.state.user.password} />
           <Button onPress={this.login} 
               buttonStyle={[{ marginBottom: 5, marginTop: 5 }]}
               title="Login"/>
--- redux/actions.js
+++ redux/actions.js
@@ -2,12 +2,12 @@
 let nextUserId = 0
 export const login = user => ({
   type: "LOGIN",
-  payload: { id: ++nextUserId, user: user }
+  payload: { id: ++nextUserId, user }
 })
 
 const initialState = { 
   type: "INITIAL_STATE",
-  payload: { id: nextUserId, user: {name: "guest", password: "guest"}}
+  payload: { id: nextUserId, user: {email: "guest", password: "guest"}}
 }
 
 // Reducer authentication
