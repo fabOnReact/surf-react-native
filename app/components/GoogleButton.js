c29e144a3751827ea3932b1a67af37c54ff765b7
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-03-15 18:20:56 +0100

    google oauth client side

---
+++ app/components/GoogleButton.js
@@ -0,0 +1,164 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, Alert, Button } from 'react-native';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+import type { User } from 'react-native-google-signin';
+import { config } from '../config/constants';
+import Api from '../lib/api_request';
+
+type ErrorWithCode = Error & { code?: string };
+
+type State = { 
+  error: ?ErrorWithCode,
+  userInfo: ?User,
+};
+
+export default class GoogleButton extends Component<{}, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: null,
+      error: null,
+    };
+  }
+
+  async componentDidMount() {
+    this._configureGoogleSignIn();
+    await this._getCurrentUser();
+  }
+
+  _configureGoogleSignIn() {
+    api = new Api()
+    api.getPosts(userCreated, authenticationFailed)
+    GoogleSignin.configure({
+      webClientId: config.webClientId,
+      offlineAccess: true,
+    });
+  }
+
+  async _getCurrentUser() {
+    try {
+      const userInfo = await GoogleSignin.signInSilently();
+      this.setState({ userInfo, error: null });
+    } catch (error) {
+      const errorMessage =
+        error.code === statusCodes.SIGN_IN_REQUIRED ? 'Please sign in :)' : error.message;
+      this.setState({
+        error: new Error(errorMessage),
+      });
+    }
+  }
+
+  render() {
+    const { userInfo } = this.state;
+
+    const body = userInfo ? this.renderUserInfo(userInfo) : this.renderSignInButton();
+    return (
+      <React.Fragment>
+        {body}
+      </React.Fragment>
+    );
+  }
+
+  renderUserInfo(userInfo) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20 }}>
+          Welcome {userInfo.user.name}
+        </Text>
+        <Text>Your user info: {JSON.stringify(userInfo.serverAuthCode)}</Text>
+
+        <Button onPress={this._signOut} title="Log out" />
+      </View>
+    );
+  }
+
+  renderIsSignedIn() {
+    return (
+      <Button
+        onPress={async () => {
+          const isSignedIn = await GoogleSignin.isSignedIn();
+          Alert.alert(String(isSignedIn));
+        }}
+        title="is user signed in?"
+      />
+    );
+  }
+
+  renderGetCurrentUser() {
+    return (
+      <Button
+        onPress={async () => {
+          const userInfo = await GoogleSignin.getCurrentUser();
+          Alert.alert('current user', userInfo ? JSON.stringify(userInfo.user) : 'null');
+        }}
+        title="get current user"
+      />
+    );
+  }
+
+  renderSignInButton() {
+    return (
+      <View style={styles.container}>
+        <GoogleSigninButton
+          style={{ width: 212, height: 48 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Auto}
+          onPress={this._signIn}
+        />
+        {this.renderError()}
+      </View>
+    );
+  }
+
+  renderError() { 
+    const { error } = this.state;
+    if (!error) {
+      return null;
+    }
+    const text = `${error.toString()} ${error.code ? error.code : ''}`;
+    return <Text>{text}</Text>;
+  }
+
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      api = new Api()
+      this.setState({ userInfo, error: null });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        Alert.alert('cancelled');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        Alert.alert('in progress');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        Alert.alert('play services not available or outdated');
+      } else {
+        Alert.alert('Something went wrong', error.toString());
+        this.setState({
+          error,
+        });
+      }
+    }
+  };
+
+  _signOut = async () => {
+    try { 
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+
+      this.setState({ userInfo: null, error: null });
+    } catch (error) {
+      this.setState({
+        error,
+      });
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
