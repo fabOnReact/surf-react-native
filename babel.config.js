3551381ad71cca0d3ef6652a72c9259d94a61d1e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-03-17 15:21:02 +0100

    jest configs

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "presets": [
+    "module:metro-react-native-babel-preset",
+     "@babel/preset-env",
+     "@babel/preset-react"
+  ],
+  "plugins": [
+    "@babel/plugin-proposal-class-properties"
+  ]
+}
