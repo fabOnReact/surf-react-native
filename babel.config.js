34d86a54b517be1eefd6e601adeca4dabd375d30
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-03-22 17:52:34 +0100

    linking project, updating babelrc

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   "presets": ["module:metro-react-native-babel-preset", "module:react-native-dotenv"],
-  "plugins": ["@babel/plugin-proposal-class-properties"],
+  "plugins": ["@babel/plugin-transform-flow-strip-types", "@babel/plugin-proposal-class-properties"],
 }
