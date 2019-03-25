428cc10e9a33c0e16b5d95e5c23d49b464850754
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-03-25 17:11:12 +0100

    configuring eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     'no-use-before-define': 'off',
     'react/jsx-filename-extension': 'off',
     'react/prop-types': 'off',
-    'comma-dangle': 'off'
+    'comma-dangle': 'off',
+    'semi': 0
   },
   'globals': {
     "fetch": false
