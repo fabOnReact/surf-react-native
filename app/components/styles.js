f2b73ec353e3e5432d01dd39aeb895396d1d4313
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-23 18:11:19 +0200

    minor fix

--- app/components/styles.js
+++ app/components/styles.js
@@ -2,6 +2,5 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  image: {},
   text: { textAlign: 'center', color: 'white', fontSize: 30, marginBottom: 5}
 });
