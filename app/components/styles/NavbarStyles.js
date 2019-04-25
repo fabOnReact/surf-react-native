eec17aa5e2022d4f1d180be39862bae1d9933219
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-25 12:17:58 +0200

    moving styles to separate files/folder

---
+++ app/components/styles/NavbarStyles.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/prefer-default-export */
+import { StyleSheet } from 'react-native';
+
+export const navbar = StyleSheet.create({
+  buttonAbsolute: {
+    position: 'absolute',
+    bottom:40,
+    right:"50%",    
+    transform: [{translateX: 30}]
+  },
+  buttonLeft: {
+    position: 'absolute',
+    bottom:35,
+    left:20,    
+  },
+  textLeft: {
+    position: 'absolute',
+    bottom:20,
+    left:25,
+    color: 'white',
+  },
+  buttonRight: {
+    position: 'absolute',
+    bottom:35,
+    right:20,    
+  },
+  textRight: {
+    position: 'absolute',
+    bottom:20,
+    right:15,
+    color: 'white',
+  },
+})
---
+++ app/screens/styles/ProfileStyles.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+import { StyleSheet } from 'react-native';
+
+export const profile = StyleSheet.create({
+  text: {
+    fontSize: 20
+  },
+})
