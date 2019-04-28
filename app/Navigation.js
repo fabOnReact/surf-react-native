7424f931cd31820b9610a4dfd0c25dcaea525615
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-04-28 19:04:33 +0200

    restoring traditional react navigation flow

--- app/Navigation.js
+++ app/Navigation.js
@@ -1,23 +1,18 @@
 import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 import AuthLoadingScreen from './screens/AuthLoadingScreen';
 import MainScreen from './screens/MainScreen';
+import CameraScreen from './screens/CameraScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import SignInScreen from './screens/SignInScreen';
 import ProfileScreen from './screens/ProfileScreen';
 
 const AppStack = createStackNavigator({ 
-  // Index: { 
-  //   screen: PostsScreen,
-  //   navigationOptions: ({navigation}) => ({
-  //     header: null,
-  //   }),
-  // },
   Main: {
     screen: MainScreen,
     navigationOptions: ({navigation}) => ({
       header: null,
     }),
-  }, Profile: ProfileScreen
+  }, New: CameraScreen, Profile: ProfileScreen
 });
 
 const AuthStack = createStackNavigator({ SignIn: SignInScreen, SignUp: SignUpScreen });
