64606fc5bf733d5787ff7d705e41dfc934ccdb54
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-05-03 15:15:13 +0200

    maps screen with basic map

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';;
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default class MapScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>Map Screen</Text>
-      </View>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
     );
   }
 }
