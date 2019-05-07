267db7dfec0f86c6ad9fcad7fef4cfa503d51a4d
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-05-07 21:00:12 +0200

    map markers clusters and query based on

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -2,14 +2,9 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import ClusteredMapView from 'react-native-maps-super-cluster';
 import { getResources } from '../lib/api';
-import Spot from '../components/Spot';
-
-const styles = StyleSheet.create({
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
+import { styles } from './styles/MapStyles';
 
 export default class MapScreen extends Component {
   static navigationOptions = {
@@ -23,10 +18,21 @@ export default class MapScreen extends Component {
   constructor(props) {
     super(props);
     const { navigation } = this.props;
-    // const lat = navigation.getParam('lat', -8)
-    // const lon = navigation.getParam('lon', 115)
-    // this.state = { data: null, latitude: lat, longitude: lon }
-    this.state = { data: null, latitude: -8.7333, longitude: 115.166 }
+    const lat = navigation.getParam('lat', -8)
+    const lon = navigation.getParam('lon', 115)
+    let empty = [{ name: '', location: { latitude: 0, longitude: 0 }}]
+    // this.state = { data: empty, latitude: lat, longitude: lon }
+    this.state = { data: empty, latitude: -8.7333, longitude: 115.166, mapBoundaries: { southWest: 0, northEast: 0 }}
+  }
+
+  componentDidMount() { 
+    this.ref.getMapRef().getMapBoundaries().then((data) => console.warn(data))
+  }
+
+  getNewCoordinates = () => {
+    this.ref.getMapRef().getMapBoundaries().then((data) => { 
+      this.setState({ mapBoundaries: data })
+    }, getResources(this.setData, this.params, "locations"))
   }
 
   componentWillMount() {
@@ -35,31 +41,76 @@ export default class MapScreen extends Component {
 
   setData = (json) => {
     const { data } = this.state
+    console.warn(json)
     this.setState({ data: json })
   }
 
   get params () {
-    const { longitude, latitude } = this.state;
-    return `?longitude=${longitude}&latitude=${latitude}`
+    const { mapBoundaries } = this.state;
+    // const { southWest, northEast } = this.state.mapBoundaries;
+    console.warn(jQuery.param(mapBoundaries))
+    return `?${jQuery.param(mapBoundaries)}`
+    // return `?south_west[latitude]=${southWest.latitude}&south_west[longitude]=${southWest.longitude}&north_east[latitude]=${northEast.latitude}&north_east[longitude]=${northEast.longitude}`
+  }
+
+  renderCluster = (cluster, onPress) => {
+    const pointCount = cluster.pointCount,
+      coordinate = cluster.coordinate,
+      clusterId = cluster.clusterId
+
+    // const clusterEngine = this.map.getClusteringEngine(),
+    //   clusteredPoints = clusteringEngine.getLeaves(clusterId, 100)
+
+    return (
+      <Marker identifier={`cluster-${clusterId}`} coordinate={coordinate} onPress={onPress}>
+        <View style={styles.clusterContainer}>
+          <Text style={styles.clusterText}>
+            {pointCount}
+          </Text>
+        </View>
+      </Marker>
+    )
+  }
+
+  renderMarker = (data) => {
+    return (
+      <Marker key={data.id || Math.random()} coordinate={data.location}>
+        <Image source={require('../images/surfboard.png')}
+          style={{ height: 30, width: 30 }}
+        />
+      </Marker>
+    )
+  }
+
+  onRegionChangeComplete = async region => {
+    let b = await this.map.getMarkersFrames();
+    console.warn(b)
+  }
+
+  getLocation = () => {
+    console.warn(this.map)
   }
 
   render() {
     const { data, latitude, longitude } = this.state;
+    const region = { 
+      latitude: latitude,
+      longitude: longitude,
+      latitudeDelta: 12,
+      longitudeDelta: 12
+    }
+
     return (
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 1,
-          longitudeDelta: 1,
-        }}
-        provider={PROVIDER_GOOGLE}
-        // showUserLocation={true}
+      <ClusteredMapView
+        style={{flex: 1}}
+        data={this.state.data}
+        initialRegion={region}
+        ref={(r) => this.ref = r}
+        renderMarker={this.renderMarker}
+        renderCluster={this.renderCluster} 
         showCompass={false}
-      >
-        { data && data.map((spot, index) => <Spot key={index} data={spot} /> )}
-      </MapView>
-    );
+        onRegionChangeComplete={this.getNewCoordinates}
+      />
+    )
   }
 }
---
+++ app/screens/styles/MapStyles.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/prefer-default-export */
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  clusterContainer: {
+    width: 30,
+    height: 30,
+    padding: 6,
+    borderWidth: 1,
+    borderRadius: 15,
+    alignItems: 'center',
+    borderColor: '#65bc46',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  clusterText: {
+    fontSize: 13,
+    color: '#65bc46',
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+})
