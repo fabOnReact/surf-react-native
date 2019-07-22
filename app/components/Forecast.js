086c58ec17cb422447697a3e3ad5c82791f83ba1
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-22 10:20:07 +0800

    Renaming header to forecast

---
+++ app/components/Forecast.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'native-base';
+import { styles } from './styles/ForecastStyles';
+
+export default class Forecast extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { data: null }
+  }
+
+  renderForecast (row) {
+    const { forecast } = row
+    return (
+      <Text key={row.id}>{ forecast && forecast.waveHeight[0].value } mt. at <Text style={{color:'red'}}>{ row.name }</Text> (<Text style={{color:'blue'}}>{ row.distance } km.</Text>) </Text>
+    )
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // console.warn(nextProps.data)
+  }
+
+  render () { 
+    const { data, index } = this.props
+    return (
+      <View style={styles.container}>
+        <Text style={{textAlign: "center"}}>{ data && index == 0 && data.map((row) => this.renderForecast(row))}</Text>
+      </View>
+    )
+  }
+}
---
+++ app/components/styles/ForecastStyles.js
@@ -0,0 +1,6 @@
+import { StyleSheet, Platform } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+  }
+})
