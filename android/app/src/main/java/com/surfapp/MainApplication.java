c45200bf584d157f423c55be387884951d0de3de
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-07-24 20:56:28 +0800

    installing permission library

--- android/app/src/main/java/com/surfapp/MainApplication.java
+++ android/app/src/main/java/com/surfapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.surfapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new LocationServicesDialogBoxPackage(),
             new RNCWebViewPackage(),
             new MapsPackage(),
