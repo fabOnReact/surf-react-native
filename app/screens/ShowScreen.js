a6648c6fceb64d837038037dc75731fb3c674c38
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-07 19:58:59 +0800

    display wave/wind info on show page

--- app/screens/ShowScreen.js
+++ app/screens/ShowScreen.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import { Text, Image } from 'react-native';
+import { View, Image } from 'react-native';
+import { H1, H2, H3, H4 } from 'native-base';
 import { GOOGLE_MAPS_API_KEY } from 'react-native-dotenv';
 import { styles } from './styles/ShowStyles';
 
 export default class ShowScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('post').location.name,
+    };
+  };
+
   render() {
     const { navigation } = this.props;
     const post = navigation.getParam('post')
     const { latitude, longitude, forecast } = post.location
-    const { windDirection, waveDirection } = forecast
+    const { swellHeight, waveHeight, windSpeed, windDirection, waveDirection } = forecast
     const host = "https://maps.googleapis.com/maps/api/staticmap"
     const options = "zoom=11&&size=300x300&maptype=satellite"
     const uri = `${host}?center=${latitude},${longitude}&${options}&key=${GOOGLE_MAPS_API_KEY}`
@@ -19,35 +26,37 @@ export default class ShowScreen extends Component {
           source={{uri: uri}} 
         />
         <Image 
-          style={{
-            position: "absolute", 
-            top: 0, 
-            left: 0,
-            width: 200, 
-            height: 200,
-            marginTop: 100,
-            marginBottom: 100,
-            marginLeft: 100,
-            marginRight: 100,
-            transform: [{ rotateZ: `${windDirection}deg`}],
-          }}
+          style={[ 
+            styles.arrow, 
+            { transform: [{ rotateZ: `${windDirection}deg`}] }
+          ]}
           source={require('../images/down-arrow.png')} 
         />
         <Image 
-          style={{
-            position: "absolute", 
-            top: 0, 
-            left: 0,
-            width: 200, 
-            height: 200,
-            marginTop: 100,
-            marginBottom: 100,
-            marginLeft: 100,
-            marginRight: 100,
-            transform: [{ rotateZ: `${waveDirection}deg`}],
-          }}
+          style={[
+            styles.arrow,
+            { transform: [{ rotateZ: `${waveDirection}deg`}] } 
+          ]}
           source={require('../images/down-cursor.png')} 
         />
+        <View style={styles.container}> 
+          <H3>{ waveHeight } mt. waves</H3>
+          <Image 
+            source={require('../images/down-cursor-black.png')}
+            style={[
+              styles.icon, 
+              { transform: [{ rotateZ: `${waveDirection}deg`}] } 
+            ]}
+          />
+          <H3>{ windSpeed } m/s wind</H3>
+          <Image 
+            source={require('../images/down-arrow-black.png')}
+            style={[ 
+                styles.icon,
+                { transform: [{ rotateZ: `${windDirection}deg`}] }
+            ]}
+          />
+        </View>
       </React.Fragment>
     )
   }
--- app/screens/styles/ShowStyles.js
+++ app/screens/styles/ShowStyles.js
@@ -1,4 +1,23 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  arrow: {
+    position: "absolute", 
+    top: 0, 
+    left: 0,
+    width: 200, 
+    height: 200,
+    marginTop: 100,
+    marginBottom: 100,
+    marginLeft: 100,
+    marginRight: 100,
+  },
+  container: {
+    height: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginTop: 10,
+  },
+  icon : { height: 30, width: 30 }
 })
