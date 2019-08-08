5de8104cb4c925b9b4d1db5496b870384f88b116
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-08 08:37:34 +0800

    react-native-chart-kit

--- android/app/src/main/java/com/surfapp/MainApplication.java
+++ android/app/src/main/java/com/surfapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.surfapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new AsyncStoragePackage(),
             new LocationServicesDialogBoxPackage(),
             new RNCWebViewPackage(),
