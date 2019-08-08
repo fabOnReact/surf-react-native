b142bb3e30d384750d04133c08722716c1f53efd
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-08 13:58:17 +0800

    stub request

--- test/fixtures/posts.js
+++ test/fixtures/posts.js
@@ -1,66 +1,23 @@
 export const posts_fixtures = [
   {
-      "id": 36,
+      "id": 48,
       "description": null,
-      "created_at": "2019-08-06 09:57:07 UTC",
-      "updated_at": "2019-08-06 09:57:07 UTC",
+      "created_at": "2019-08-08 01:09:18 UTC",
+      "updated_at": "2019-08-08 01:09:18 UTC",
       "user_id": 2,
       "picture": {
-          "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/2019-08-06T09_56_53.509Z.png",
+          "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/2019-08-08T01_08_49.951Z.png",
           "thumb": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/thumb_2019-08-06T09_56_53.509Z.png"
+              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/thumb_2019-08-08T01_08_49.951Z.png"
           },
           "card": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/card_2019-08-06T09_56_53.509Z.png"
+              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/card_2019-08-08T01_08_49.951Z.png"
           },
           "web": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/web_2019-08-06T09_56_53.509Z.png"
+              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/web_2019-08-08T01_08_49.951Z.png"
           },
           "mobile": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/mobile_2019-08-06T09_56_53.509Z.png"
-          }
-      },
-      "latitude": 37.785834,
-      "longitude": -122.406417,
-      "city": null,
-      "location": {
-          "name": "Fort Point",
-          "forecast": {
-              "time": "2019-08-07T01:00:00+00:00",
-              "swellHeight": 0.71,
-              "waveHeight": 1.99,
-              "windSpeed": 2.95,
-              "windDirection": 247.38,
-              "waveDirection": 301.79,
-              "swellDirection": 285.15,
-              "swellPeriod": 6.78
-          },
-          "latitude": 37.810665,
-          "longitude": -122.476358
-      },
-      "liked": false,
-      "date": "about 16 hours ago",
-      "url": "http://192.168.100.14:3000/posts/36.json"
-  },
-  {
-      "id": 35,
-      "description": null,
-      "created_at": "2019-08-06 09:57:06 UTC",
-      "updated_at": "2019-08-06 09:57:06 UTC",
-      "user_id": 2,
-      "picture": {
-          "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/2019-08-06T09_56_53.856Z.png",
-          "thumb": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/thumb_2019-08-06T09_56_53.856Z.png"
-          },
-          "card": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/card_2019-08-06T09_56_53.856Z.png"
-          },
-          "web": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/web_2019-08-06T09_56_53.856Z.png"
-          },
-          "mobile": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/mobile_2019-08-06T09_56_53.856Z.png"
+              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/mobile_2019-08-08T01_08_49.951Z.png"
           }
       },
       "latitude": 37.785834,
@@ -68,107 +25,93 @@ export const posts_fixtures = [
       "city": "San Francisco",
       "location": {
           "name": "Fort Point",
-          "forecast": {
-              "time": "2019-08-07T01:00:00+00:00",
-              "swellHeight": 0.71,
-              "waveHeight": 1.99,
-              "windSpeed": 2.95,
-              "windDirection": 247.38,
-              "waveDirection": 301.79,
-              "swellDirection": 285.15,
-              "swellPeriod": 6.78
-          },
           "latitude": 37.810665,
-          "longitude": -122.476358
-      },
-      "liked": false,
-      "date": "about 16 hours ago",
-      "url": "http://192.168.100.14:3000/posts/35.json"
-  },
-  {
-      "id": 34,
-      "description": null,
-      "created_at": "2019-08-06 09:57:03 UTC",
-      "updated_at": "2019-08-06 09:57:03 UTC",
-      "user_id": 2,
-      "picture": {
-          "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/2019-08-06T09_56_52.574Z.png",
-          "thumb": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/thumb_2019-08-06T09_56_52.574Z.png"
+          "longitude": -122.476358,
+          "tide": {
+              "hours": [
+                  "2019-08-08T01:00:00+00:00",
+                  "2019-08-08T02:00:00+00:00",
+                  "2019-08-08T03:00:00+00:00",
+                  "2019-08-08T04:00:00+00:00",
+                  "2019-08-08T05:00:00+00:00",
+                  "2019-08-08T06:00:00+00:00",
+                  "2019-08-08T07:00:00+00:00",
+                  "2019-08-08T08:00:00+00:00",
+                  "2019-08-08T09:00:00+00:00",
+                  "2019-08-08T10:00:00+00:00",
+                  "2019-08-08T11:00:00+00:00",
+                  "2019-08-08T12:00:00+00:00",
+                  "2019-08-08T13:00:00+00:00",
+                  "2019-08-08T14:00:00+00:00",
+                  "2019-08-08T15:00:00+00:00",
+                  "2019-08-08T16:00:00+00:00",
+                  "2019-08-08T17:00:00+00:00",
+                  "2019-08-08T18:00:00+00:00",
+                  "2019-08-08T19:00:00+00:00",
+                  "2019-08-08T20:00:00+00:00",
+                  "2019-08-08T21:00:00+00:00",
+                  "2019-08-08T22:00:00+00:00",
+                  "2019-08-08T23:00:00+00:00",
+                  "2019-08-09T00:00:00+00:00",
+                  "2019-08-09T01:00:00+00:00"
+              ],
+              "seaLevels": [
+                  1.81,
+                  1.62,
+                  1.29,
+                  0.91,
+                  0.56,
+                  0.34,
+                  0.25,
+                  0.3,
+                  0.46,
+                  0.68,
+                  0.92,
+                  1.13,
+                  1.27,
+                  1.29,
+                  1.2,
+                  1.03,
+                  0.85,
+                  0.73,
+                  0.71,
+                  0.81,
+                  1.01,
+                  1.27,
+                  1.53,
+                  1.75,
+                  1.86
+              ]
           },
-          "card": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/card_2019-08-06T09_56_52.574Z.png"
+          "hourly": {
+              "time": "2019-08-08T01:00:00+00:00",
+              "windSpeed": 5.25,
+              "waveHeight": 0.92,
+              "swellHeight": 0.87,
+              "swellPeriod": 8.7,
+              "waveDirection": 259.76,
+              "windDirection": 261.1,
+              "swellDirection": 259.24
           },
-          "web": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/web_2019-08-06T09_56_52.574Z.png"
-          },
-          "mobile": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/mobile_2019-08-06T09_56_52.574Z.png"
+          "daily": {
+              "days": [
+                  "Wednesday",
+                  "Thursday",
+                  "Friday",
+                  "Saturday",
+                  "Sunday"
+              ],
+              "waveHeight": [
+                  1.2,
+                  0.8,
+                  0.7,
+                  0.9,
+                  1.2
+              ]
           }
       },
-      "latitude": 37.785834,
-      "longitude": -122.406417,
-      "city": "San Francisco",
-      "location": {
-          "name": "Fort Point",
-          "forecast": {
-              "time": "2019-08-07T01:00:00+00:00",
-              "swellHeight": 0.71,
-              "waveHeight": 1.99,
-              "windSpeed": 2.95,
-              "windDirection": 247.38,
-              "waveDirection": 301.79,
-              "swellDirection": 285.15,
-              "swellPeriod": 6.78
-          },
-          "latitude": 37.810665,
-          "longitude": -122.476358
-      },
       "liked": false,
-      "date": "about 16 hours ago",
-      "url": "http://192.168.100.14:3000/posts/34.json"
-  },
-  {
-      "id": 32,
-      "description": null,
-      "created_at": "2019-08-06 01:11:06 UTC",
-      "updated_at": "2019-08-06 01:11:06 UTC",
-      "user_id": 2,
-      "picture": {
-          "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/2019-08-06T01_10_38.069Z.png",
-          "thumb": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/thumb_2019-08-06T01_10_38.069Z.png"
-          },
-          "card": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/card_2019-08-06T01_10_38.069Z.png"
-          },
-          "web": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/web_2019-08-06T01_10_38.069Z.png"
-          },
-          "mobile": {
-              "url": "https://s3.eu-central-1.amazonaws.com/surfcheck/uploads/post/2019/8/2/mobile_2019-08-06T01_10_38.069Z.png"
-          }
-      },
-      "latitude": 37.785834,
-      "longitude": -122.406417,
-      "city": "San Francisco",
-      "location": {
-          "name": "Fort Point",
-          "forecast": {
-              "time": "2019-08-07T01:00:00+00:00",
-              "swellHeight": 0.71,
-              "waveHeight": 1.99,
-              "windSpeed": 2.95,
-              "windDirection": 247.38,
-              "waveDirection": 301.79,
-              "swellDirection": 285.15,
-              "swellPeriod": 6.78
-          },
-          "latitude": 37.810665,
-          "longitude": -122.476358
-      },
-      "liked": false,
-      "date": "1 day ago",
-      "url": "http://192.168.100.14:3000/posts/32.json"
+      "date": "39 minutes ago",
+      "url": "http://192.168.43.59:3000/posts/48.json"
   }
-]
+] 
