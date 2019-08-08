f5200b07a7ed7205a96f034e224ed7ce23d081e0
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-08 13:18:47 +0800

    tide chart

--- app/components/Tide.js
+++ app/components/Tide.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
+import { H1, H2, H3, H4 } from 'native-base';
 import { LineChart } from 'react-native-chart-kit'
 import { Dimensions } from 'react-native'
 
 export default class Tide extends Component {
   render() {
+    var { hours, seaLevels } = this.props.data
     const chartConfig = {
-      backgroundGradientFrom: '#1E2923',
-      backgroundGradientTo: '#08130D',
-      color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-      strokeWidth: 2,
+      backgroundGradientFrom: 'white',
+      backgroundGradientTo: 'white',
+      color: (opacity = 1) => `rgba(23, 79, 23)`,
+      decimalPlaces: 0,
+      strokeWidth: 0,
     }
-    const screenWidth = Dimensions.get('window').width
+    const screenWidth = Dimensions.get('window').width - 50
+    const beginning = new Date(hours[0]).getHours()
+    const end = new Date(hours.slice(-1)).getHours()
+    hours  = hours.map(date => new Date(date).getHours()).filter(hour => hour % 3 == 0) 
+    const labels = hours.map(( time) => new Date(time).getHours())
     const data = {
-      labels: ['10', '11', '12', '13', '14', '15'],
+      labels: hours,
       datasets: [{
-        data: [ 2, 4, 2, 0, 1, 0],
-        color: (opacity = 1) => `rgba(25, 121, 56, ${opacity})`, // optional
-        strokeWidth: 2 // optional
+        data: seaLevels,
+        strokeWidth: 2 
       }]
     }
     return (
-      <LineChart
-        data={data}
-        width={screenWidth}
-        height={220}
-        chartConfig={chartConfig}
-        bezier
-      />
+      <React.Fragment>
+        <H3 style={{ textAlign: 'center', marginTop: 10 }}>Next 24h Tide mt.</H3>
+        <LineChart
+          data={data}
+          width={screenWidth}
+          height={180}
+          chartConfig={chartConfig}
+          withInnerLines={false}
+          withOuterLines={false}
+          withDots={false}
+        />
+      </React.Fragment>
     )
   }
 }
--- app/screens/ShowScreen.js
+++ app/screens/ShowScreen.js
@@ -1,65 +1,70 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { ScrollView, View, Image } from 'react-native';
 import { H1, H2, H3, H4 } from 'native-base';
 import Tide from '../components/Tide';
 import Waves from '../components/Waves';
 import { GOOGLE_MAPS_API_KEY } from 'react-native-dotenv';
 import { styles } from './styles/ShowStyles';
+import { posts_fixtures } from '../../test/fixtures/posts.js';
 
 export default class ShowScreen extends Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.getParam('post').location.name,
-    };
-  };
+  // static navigationOptions = ({ navigation }) => {
+  //   return {
+  //     title: navigation.getParam('post').location.name,
+  //   };
+  // };
 
   render() {
     const { navigation } = this.props;
-    const post = navigation.getParam('post')
+    // const post = navigation.getParam('post')
+    const post = posts_fixtures[0]
     const { latitude, longitude, tide, daily, hourly } = post.location
     const { swellHeight, waveHeight, windSpeed, windDirection, waveDirection } = hourly
     const host = "https://maps.googleapis.com/maps/api/staticmap"
-    const options = "zoom=11&&size=300x300&maptype=satellite"
+    const options = "zoom=11&&size=400x300&maptype=satellite"
     const uri = `${host}?center=${latitude},${longitude}&${options}&key=${GOOGLE_MAPS_API_KEY}`
     return (
       <React.Fragment>
-        <Image 
-          style={{width: 400, height: 400}}
-          source={{uri: uri}} 
-        />
-        <Image 
-          style={[ 
-            styles.arrow, 
-            { transform: [{ rotateZ: `${windDirection}deg`}] }
-          ]}
-          source={require('../images/down-arrow.png')} 
-        />
-        <Image 
-          style={[
-            styles.arrow,
-            { transform: [{ rotateZ: `${waveDirection}deg`}] } 
-          ]}
-          source={require('../images/down-cursor.png')} 
-        />
-        <View style={styles.container}> 
-          <H3>{ waveHeight } mt. waves</H3>
+        <ScrollView>
           <Image 
-            source={require('../images/down-cursor-black.png')}
+            style={{width: 400, height: 300}}
+            source={{uri: uri}} 
+          />
+          <Image 
+            style={[ 
+              styles.arrow, 
+              { transform: [{ rotateZ: `${windDirection}deg`}] }
+            ]}
+            source={require('../images/down-arrow.png')} 
+          />
+          <Image 
             style={[
-              styles.icon, 
+              styles.arrow,
               { transform: [{ rotateZ: `${waveDirection}deg`}] } 
             ]}
+            source={require('../images/down-cursor.png')} 
           />
-          <H3>{ windSpeed } m/s wind</H3>
-          <Image 
-            source={require('../images/down-arrow-black.png')}
-            style={[ 
-                styles.icon,
-                { transform: [{ rotateZ: `${windDirection}deg`}] }
-            ]}
-          />
-        </View>
-        <Tide />
+          <View style={styles.container}> 
+            <H3>{ waveHeight } mt. waves</H3>
+            <Image 
+              source={require('../images/down-cursor-black.png')}
+              style={[
+                styles.icon, 
+                { transform: [{ rotateZ: `${waveDirection}deg`}] } 
+              ]}
+            />
+            <H3>{ windSpeed } m/s wind</H3>
+            <Image 
+              source={require('../images/down-arrow-black.png')}
+              style={[ 
+                  styles.icon,
+                  { transform: [{ rotateZ: `${windDirection}deg`}] }
+              ]}
+            />
+          </View>
+          <Tide data={post.location.tide} />
+          <Waves data={post.location.daily} />
+        </ScrollView>
       </React.Fragment>
     )
   }
