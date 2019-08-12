0b358f36e2df33fba4858f161039f95ffa7d74f5
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-12 21:28:26 +0800

    updating ip

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,2 +1,2 @@
-// export const host = 'https://surfcheck.xyz'
-export const host = 'http://192.168.0.100:3000'
+export const host = 'https://surfcheck.xyz'
+// export const host = 'http://192.168.0.100:3000'
