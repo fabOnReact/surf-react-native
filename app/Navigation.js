98fb999ddec5de6847dca29157b15a595fbc4a2c
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-21 21:11:24 +0800

    configuring navigation with new screens

--- app/Navigation.js
+++ app/Navigation.js
@@ -7,16 +7,19 @@ import SignInScreen from './screens/SignInScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import MapScreen from './screens/MapScreen';
 import WebViewScreen from './screens/WebViewScreen';
-import ShowScreen from './screens/ShowScreen';
+import ForecastScreen from './screens/ForecastScreen';
+import NearbyScreen from './screens/NearbyScreen';
 
 const AppStack = createStackNavigator({ 
+  Nearby: NearbyScreen,
   Index: {
     screen: IndexScreen,
     navigationOptions: ({navigation}) => ({
       header: null,
     }),
-  }, Camera: CameraScreen, Profile: ProfileScreen, Map: MapScreen, WebView: WebViewScreen,
-  Show: ShowScreen
+  }, Camera: CameraScreen, Profile: ProfileScreen, Map: MapScreen, 
+     WebView: WebViewScreen,
+     Forecast: ForecastScreen,
 });
 
 const AuthStack = createStackNavigator({ SignIn: SignInScreen, SignUp: SignUpScreen });
