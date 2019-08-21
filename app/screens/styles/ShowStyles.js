a8bf8a7231e8c4d63d129bec3543ae28dee12db0
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-21 21:11:13 +0800

    Moving styles to child

--- app/screens/styles/ShowStyles.js
+++ app/screens/styles/ShowStyles.js
@@ -1,23 +1,4 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  arrow: {
-    position: "absolute", 
-    top: 0, 
-    left: 0,
-    width: 200, 
-    height: 200,
-    marginTop: 50,
-    marginBottom: 50,
-    marginLeft: 100,
-    marginRight: 100,
-  },
-  container: {
-    height: 30,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    marginTop: 10,
-  },
-  icon : { height: 30, width: 30 }
 })
