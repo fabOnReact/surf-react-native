1e345ce89eb72e0297621f6750e7337e322ee470
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-21 21:07:58 +0800

    configuring android react-native-video

--- android/app/src/main/java/com/surfapp/MainApplication.java
+++ android/app/src/main/java/com/surfapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.surfapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.horcrux.svg.SvgPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new RNCameraPackage(),
             new SvgPackage(),
             new AsyncStoragePackage(),
