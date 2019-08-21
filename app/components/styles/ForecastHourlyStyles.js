b16430f918dc48ee9c465735b10a1c64c3d7d9f0
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-21 21:10:22 +0800

    refactoring Forecast Screen

---
+++ app/components/styles/ForecastHourlyStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    marginTop: 10,
+  },
+  icon : { height: 30, width: 30 }
+})
---
+++ app/components/styles/ForecastMapStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  arrow: {
+    position: "absolute", 
+    top: 0, 
+    left: 0,
+    width: 200, 
+    height: 200,
+    marginTop: 50,
+    marginBottom: 50,
+    marginLeft: 100,
+    marginRight: 100,
+  },
+})
