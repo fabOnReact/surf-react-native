50f75252e09738285e11a1a7d32a9e0b9b4e7be5
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-08-27 22:37:09 +0800

    fixing merge conflicts

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,4 +1,4 @@
-// export const host = 'https://surfcheck.xyz'
-export const host = 'http://192.168.0.100:3000'
-// export const host = 'http://192.168.43.59:3000'
-// export const host = "https://d502b9f2-3144-4162-b6d6-1ce798aafc39.mock.pstmn.io"
+export const host = 'https://surfcheck.xyz/api/v1'
+// export const host = 'http://192.168.0.100:3000/api/v1'
+// export const host = 'http://192.168.43.59:3000/api/v1'
+// export const host = "https://d502b9f2-3144-4162-b6d6-1ce798aafc39.mock.pstmn.io/api/v1"
