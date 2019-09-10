b65a7807882a9a98bdabeb2172373ed452f1ae6e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-09-10 22:12:39 +0800

    fixing Post

--- app/components/Post.js
+++ app/components/Post.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import { Header } from 'react-navigation';
 import Video from 'react-native-video';
 import Dimensions from 'Dimensions';
 import Orientation from 'react-native-orientation';
-import { updatePost } from '../lib/api';
-import ForecastInfo from '../components/ForecastInfo';
-import { host } from '../config/constants';
-import { errorMessage, isPresent } from '../lib/support';
-import { styles } from  './styles/PostStyles';
+import ForecastInfo from './ForecastInfo';
 import { getAsset } from '../lib/support';
 
 export default class Post extends Component {
   constructor(props) {
     super(props)
-    this.state = { liked: null, style: '', portrait: null, width: null, height: null, video_height: 0 }
+    this.state = { portrait: null, width: null, height: null }
   }
 
   componentWillMount() { Orientation.addOrientationListener(this._imageStyles) } 
-  componentWillUnmount() { Orientation.removeOrientationListener(this._imageStyles) }
   componentDidMount() { this._imageStyles("PORTRAIT") }
+  componentWillUnmount() { Orientation.removeOrientationListener(this._imageStyles) }
+
+  get style() { 
+    const { height, width, portrait } = this.state
+    const style = { flex: 2, borderRadius: 9, width: null, height:null }
+    switch(portrait) {
+      case true:
+        return { ...style, height: height };
+        break;
+      case false:
+        return { ...style, height: width };
+        break;
+      default:
+        // console.log("impossible to choose correct style")
+    }
+  }
 
   _imageStyles = (orientation) => {
     const new_width = Dimensions.get('window').width
@@ -27,28 +38,12 @@ export default class Post extends Component {
       this.setState({ width: new_width - Header.HEIGHT/2, portrait: false })
     } else { 
       const new_height = (Dimensions.get('window').height - Header.HEIGHT)/3
-      this.setState({ width: new_width, height: new_height, video_height: new_height - 40, portrait: true })
-    }
-  }
-
-  get style() { 
-    const { height, width, portrait } = this.state
-    const style = { flex: 2, borderRadius: 9, width: null, height:null }
-    switch(portrait) {
-      case true:
-        return { ...style, height: height }
-        break
-      case false:
-        return { ...style, height: width }
-        break
-      default:
-        // console.log("impossible to choose correct style")
+      this.setState({ width: new_width, height: new_height, portrait: true })
     }
   }
 
   render() {
-    const { height, video_height, width } = this.state
-    const { navigation, post, index } = this.props
+    const { navigation, post } = this.props
     const { location } = post
     const { forecast_info } = location
     var video_source, image_source
@@ -61,22 +56,29 @@ export default class Post extends Component {
         <TouchableOpacity 
           onPress={() => { 
             if (forecast_info) { navigation.navigate('Forecast', { location: location, post: post }) }
-        }}>
+          }}
+        >
           { 
-            !!post.picture.url && <Image 
-            source={{uri: post.picture.mobile.url }} 
-            style={this.style} /> 
+            !!post.picture.url && ( 
+              <Image 
+                source={{uri: post.picture.mobile.url }} 
+                style={this.style} 
+              /> 
+            )
           }
 
           { 
-            !!post.video && <Video 
-            source={video_source}
-            poster={image_source}
-            posterResizeMode={"cover"}
-            resizeMode={"cover"}
-            style={{ borderRadius: 9, aspectRatio: 1.7, width: "100%" }}
-            repeat 
-            muted />
+            !!post.video && (
+              <Video 
+                source={video_source}
+                poster={image_source}
+                posterResizeMode="cover"
+                resizeMode="cover"
+                style={{ borderRadius: 9, aspectRatio: 1.7, width: "100%" }}
+                repeat 
+                muted 
+              />
+            )
           }
 
           <ForecastInfo location={location} />
