d8907a072e51318b4d2f13a38227f0c87df6e488
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-09 16:04:56 +0800

    Video Camera functionalities

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,7 +1,57 @@
 import { GoogleSignin } from 'react-native-google-signin';
+import AsyncStorage from '@react-native-community/async-storage';
 import { WEB_CLIENT_ID, IOS_CLIENT_ID } from 'react-native-dotenv';
 import { host } from '../config/constants';
-import { getFromStorage, errorMessage, getCredentials, headers } from './support';
+import { postSettings, getFromStorage, errorMessage, getCredentials, headers } from './support';
+
+export default class Api {
+  get post() {
+    return { 
+      responseStatus: 201,
+    }
+  }
+
+  constructor() {
+    this.setCredentials()
+  }
+
+  setCredentials = async () => {
+    this._credentials = { 
+      'X-User-Email': await AsyncStorage.getItem('userEmail'),
+      'X-User-Token': await AsyncStorage.getItem('userToken'),
+    } 
+    return await this._credentials
+  }
+
+  getConfig =  async (method, body) => {
+    const config = { method, body }
+    if (!this._credentials) {
+      this._credentials = await this.setCredentials()
+    }
+    config["headers"] = {
+      ...headers,
+      ...this._credentials,
+    }
+    return config
+  }
+
+  getLocations = async (latitude, longitude) => {
+    const config = await this.getConfig("GET")
+    const gps = `latitude=${latitude}&longitude=${longitude}`
+    const pages = `page=1&per_page=1`
+    return await fetch(`${host}/locations.json?${gps}&${pages}`, config)
+  }
+
+  createPost = async (data) => {
+    const config = await this.getConfig("POST", data) 
+    return await fetch(`${host}/posts.json`, config)
+  }
+
+  updatePost = async (id, data) => {
+    const config = await this.getConfig("PUT", data)
+    return await fetch(`${host}/posts/${id}.json`, config)
+  }
+}
 
 export const createResource = (success, failure, body, settings) => {
   const options = { method: 'POST', headers: headers, body: body }
@@ -16,30 +66,21 @@ export const createResource = (success, failure, body, settings) => {
     .catch(error => errorMessage(error));
 }
 
-export const createPost = async (success, data, settings) => {
-  let credentials = await getCredentials()
-  const config = { method: 'POST', body: data }
-  headers['Content-Type'] = "multipart/form-data;"
-  config["headers"] = { 
-    ...headers, 
-    ...credentials, 
-    "accept-encoding": "gzip, deflate"
-  }
-
-  fetch(`${host}/${settings.endpoint}.json`, config)
-    .then(response => { 
-      response.json().then(data => success(data, response.status))
-    })
-    .catch(error => errorMessage(error))
-}
-
 export const getResources = async (success, path) => {
   let credentials = await getCredentials()
   let config = { method: 'GET', headers: {...headers, ...credentials} }
   fetch(`${host}/${path}`, config)
-    .then(response => response.json())
-    .then(json => success(json))
-    .catch(error => errorMessage(error))
+     .then(response => response.json())
+     .then(json => success(json))
+     .catch(error => errorMessage(error))
+}
+
+export const uploadVideo = async (data) => {
+  const endpoint = "https://api.cloudinary.com/v1_1/dhhzzcjq0/video/upload"
+  return await fetch(endpoint, {
+    method: 'post',
+    body: data
+  });
 }
 
 export const getGoogleUser = async (success, failure) => {
--- app/screens/CameraScreen.js
+++ app/screens/CameraScreen.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Alert, StyleSheet, TouchableOpacity, TouchableHighlight, View, Text, StatusBar, ImageBackground } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import Spinner from 'react-native-loading-spinner-overlay';
 import Dimensions from 'Dimensions';
 import { RNCamera } from 'react-native-camera';
 import Geolocation from 'react-native-geolocation-service';
@@ -8,7 +10,9 @@ import ErrorMessage from '../components/ErrorMessage'
 import Orientation from 'react-native-orientation-locker';
 import Location from '../components/Location';
 import { buttons } from '../components/styles/ButtonStyles';
-import { createPost } from '../lib/api';
+import { createPost, uploadVideo, getLocations } from '../lib/api';
+import Api from '../lib/api';
+import Tutor from '../components/Tutor';
 import { errorMessage, postSettings } from '../lib/support';
 import ClientDate from '../lib/client_date';
 
@@ -23,9 +27,12 @@ export default class CameraScreen extends Component {
 
   constructor(props) {
     super(props)
-    this._takePicture = this._takePicture.bind(this)
     this._setLocation()
-    this.state = { latitude: null, longitude: null }
+    this.state = { latitude: null, longitude: null, recording: false, processing: false, highlight: false, processing: false, locations: [], url: null, poster: null, tutor: true }
+  }
+
+  componentDidMount() {
+    this._checkLocation()
   }
 
   _setLocation = function() {
@@ -43,64 +50,151 @@ export default class CameraScreen extends Component {
     );
   }
 
-  success = (json, status) => {
-    if (status != postSettings.responseStatus) { 
-      Alert.alert(
-        'Sorry! Picture was not saved!',
-        json["location"][1],
-        [
-          {text: 'OK', onPress: () => {}},
-        ],
-        {cancelable: false},
-      );
+  get options() {
+    return {
+        quality: 0.5,
+        fixOrientation: true,
+        forceUpOrientation: true,
+        videoOrientation: 1,
+        deviceOrientation: 1,
+        mute: true,
+        mirrorVideo: false,
+        maxDuration: 300,
+      }
+  }
+
+  get post() {
+    const { latitude, longitude, url = "", poster = "" } = this.state
+    return JSON.stringify({ 
+      post: { 
+        latitude, 
+        longitude, 
+        video: { url, poster }
+      }
+    })
+  }
+
+  _checkLocation = async () => {
+    const { latitude, longitude } = this.state
+    this._api = new Api()
+    var response = await this._api.getLocations(latitude, longitude)
+    var location = await response.json()[0]
+    this.setState({ location })
+  }
+
+  _startRecording = async function() {
+    const { recording, highlight } = this.state;
+    this.setState({ recording: !recording });
+  }
+
+  componentDidUpdate = async (prevProp, prevState) => {
+    const { recording, processing } = this.state
+    const is_recording = prevState.recording == false && recording
+    const is_processing = prevState.processing == false && processing
+    const processing_finished = prevState.processing == true && !processing
+    if (is_recording) {
+      this.interval = setInterval(() => this.setState({
+        highlight: !this.state.highlight
+      }), 900)
+      this._video  = await this.camera.recordAsync(this.options)
+      this.setState({ processing: true })
+    }
+    if (is_processing) {
+      var response = await uploadVideo(this.data)
+      var json = await response.json()
+      const { secure_url, format } = json
+      const url = secure_url.replace(format, "mp4")
+      const poster = secure_url.replace(format, "png")
+      this.setState({ processing: false, url, poster })
+    }
+    if (processing_finished) {
+      this._api.createPost(this.post)
+      this.setState({ url: null, poster: null, post: null })
     }
   }
 
-  _takePicture = async function() {
-    const { navigation } = this.props
-    const alert = navigation.getParam('alertCallback')
-    const { latitude, longitude } = this.state;
-    const options = { quality: 0.5, base64: true, forceUpOrientation: true, fixOrientation: true };
-    const picture = await this.camera.takePictureAsync(options);
-    const data = new FormData();
-    const timestamp = new ClientDate().iso;
-    data.append('post[picture][file]', picture.base64);
-    data.append('post[picture][name]', `${timestamp}.png`);
-    data.append('post[picture][type]', 'image/png');
-    data.append('post[latitude]', latitude);
-    data.append('post[longitude]', longitude);
-    await createPost(this.success, data, postSettings)
-  };
+  get data() {
+    const { uri, codec = "mp4" } = this._video
+    const type = `video/${codec}`
+    const data = new FormData()
+    data.append("file", { 
+      name: 'my_video', 
+      uri,
+      type
+    })
+    data.append("upload_preset", "azmsaq1v")
+    return data
+  }
+
+  get message() {
+    return `Looks like you are very far from any surf destination, 
+            only videos that are taken at a surfspot present 
+            in our database are accepted. Sorry!`
+  }
+
+  _recording = () => {
+    const { recording, location } = this.state
+    if (!!location) { 
+      alert(this.message) 
+      return
+    }
+    recording ? this._stopRecording() : this._startRecording()
+  }
+
+  _stopRecording = async () => {
+    clearInterval(this.interval) 
+    this.setState({ highlight: false, recording: false })
+    this.camera.stopRecording()
+  }
+
+  hideTutor = () => {
+    AsyncStorage.setItem('tutor', "false")
+    this.setState({ tutor: false })
+  }
+
+  _renderCamera() {
+    const { recording, highlight, processing } = this.state
+    return (
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        captureAudio={false}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}>
+          <Icon
+            containerStyle={[buttons.buttonAbsolute, {borderRadius: 10}]}
+            name='ios-radio-button-on'
+            type='ionicon'
+            size={80}
+            color={ highlight ? 'red' : '#ffffff' }
+            underlayColor='transparent'
+            disabled={processing}
+            disabledStyle={{backgroundColor: "transparent"}}
+            onPress={this._recording}
+          />
+      </RNCamera>
+    )
+  }
 
   render() {
-    const { errors } = this.state
+    const { errors, tutor, processing } = this.state
     return (
       <View style={styles.container}>
+        <Spinner
+          visible={processing}
+          textContent={'Uploading...'}
+          textStyle={styles.spinnerTextStyle}
+        />
         { errors ? <ErrorMessage styles={{marginTop: 100}} message={errors} /> : null }
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          captureAudio={false}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}>
-            <Icon
-              containerStyle={[buttons.buttonAbsolute, {borderRadius: 10}]}
-              name='ios-radio-button-on'
-              type='ionicon'
-              size={80}
-              color='#ffffff'
-              underlayColor='transparent'
-              onPress={this._takePicture}
-            />
-        </RNCamera>
+        { tutor ? <Tutor hide={this.hideTutor} /> : this._renderCamera() }
       </View>
     );
   }
