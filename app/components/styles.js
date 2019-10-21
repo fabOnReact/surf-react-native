5322926deab48bf2ccf12a9fc1543e6ce562f609
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-21 08:01:07 +0800

    removing styles component

--- app/components/styles.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { StyleSheet } from 'react-native';
-
-export const styles = StyleSheet.create({
-  text: { textAlign: 'center', color: 'white', fontSize: 30, marginBottom: 5}
-});
