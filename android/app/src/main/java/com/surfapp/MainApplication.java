4e5588b4afc04e692f42a84bd42cd008998b7e22
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-23 12:42:10 +0800

    configuring transparent nav buttons on android

--- android/app/src/main/java/com/surfapp/MainApplication.java
+++ android/app/src/main/java/com/surfapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.surfapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.thebylito.navigationbarcolor.NavigationBarColorPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.bugsnag.BugsnagReactNative;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NavigationBarColorPackage(),
             new RNDeviceInfo(),
             new RNScreensPackage(),
             BugsnagReactNative.getPackage(),
