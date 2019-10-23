ba0002f5b25be6718016e91470f08a06a512b442
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-23 12:40:56 +0800

    safe area to manage different screens

---
+++ app/components/SafeArea.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, Dimensions, Platform } from 'react-native'
+import { Header, SafeAreaView } from 'react-navigation';
+
+export default class SafeArea extends Component {
+  render() {
+    const { style } = this.props
+    return (
+      <View
+        style={[
+          styles.safe_area,
+          style
+        ]}>
+        { this.props.children }
+      </View>
+    )
+  }
+}
+
+const is_ios = Platform.OS === 'ios'
+let deviceH = Dimensions.get('screen').height;
+let windowH = Dimensions.get('window').height;
+let bottomNavBarH = deviceH - windowH;
+
+const styles = StyleSheet.create({
+  safe_area: {
+    zIndex: 0,
+    position: 'absolute',
+    bottom: is_ios ? 7 : bottomNavBarH,
+    left: 0,
+    right: 0,
+  }
+})
