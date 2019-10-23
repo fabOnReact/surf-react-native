3fd1c97f7b46ea76beec980c045b3803014f5dba
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-23 08:58:17 +0800

    configuring forecast screen

--- app/screens/ForecastScreen.js
+++ app/screens/ForecastScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StatusBar, Platform, Dimensions, ScrollView, Image } from 'react-native';
+import { Orientation, StatusBar, Platform, Dimensions, ScrollView, Image } from 'react-native';
 import { Header } from 'react-navigation';
 import { H3 } from 'native-base';
 import Video from 'react-native-video';
@@ -11,41 +11,40 @@ import { getAsset } from '../lib/support';
 export default class ForecastScreen extends Component {
   static navigationOptions = ({ navigation }) => {
     const { params = {} } = navigation.state
-    const post = navigation.getParam('post')
-    if (!!post && Platform.OS == "ios") {
-      return {
-        title: navigation.getParam('location').name,
-        headerTintColor: params.showHeader ? 'black' : 'white',
-        headerTransparent: params.showHeader ? false : true,
-        headerStyle: { borderBottomWidth: 0, }
-      };
-    } else if(!!post & Platform.OS != "ios") {
-      return {
-        title: navigation.getParam('location').name,
-        headerTintColor: params.showHeader ? 'black' : 'white',
-        headerTransparent: params.showHeader ? false : true,
-        headerStyle: { borderBottomWidth: 0, marginTop: 10 } 
-      };
-    } else {
-      return {
-        title: navigation.getParam('location').name,
-        headerTintColor: 'black' ,
-        headerTransparent: false,
-      };
-    }
+    const location = navigation.getParam('location')
+    const { data: { attributes: { name }}} = location
+    return {
+      title: name,
+      headerTintColor: 'black' ,
+      headerTransparent: false,
+    };
   };
 
   constructor(props) {
     super(props)
     this.state = { showHeader: false }
+    this.location = this.props.navigation.getParam('location')
   }
 
-  componentWillMount() {
-    const { navigation } = this.props
-    navigation.setParams({
-      showHeader: false
-    })
-  } 
+  componentWillMount = async () => {
+    // const { navigation } = this.props
+    // navigation.setParams({
+    //   showHeader: false
+    // })
+    // Orientation.addOrientationListener(this._imageStyles) 
+    // const { included: { attributes: cameras }} = this.location
+    // this.setState({ camera: cameras[0] })
+  }
+
+  //  _imageStyles = (orientation) => {
+  //    const new_width = this.width - Header.HEIGHT/2
+  //    if (orientation != 'PORTRAIT') { 
+  //      this.setState({ width: new_width, portrait: false })
+  //    } else { 
+  //      const new_height = (this.height - Header.HEIGHT)/3
+  //      this.setState({ width: new_width, height: new_height, portrait: true })
+  //    }
+  //  }
 
   componentDidMount() {
     const height = Dimensions.get('window').height
@@ -65,41 +64,23 @@ export default class ForecastScreen extends Component {
   render() {
     const { navigation } = this.props;
     const { showHeader } = this.state
-    const location = navigation.getParam('location')
-    const post = navigation.getParam('post', null)
-    const { forecast_info } = location
+    // const post = navigation.getParam('post', null)
+    // console.error(this.location);
+    const { data: { attributes: location_attributes }} = this.location
+    const { forecast_info } = location_attributes
     const { daily } = forecast_info
+    const { included: cameras } = this.location
+    // const { attributes: camera } = cameras[0]
+    // const { posts } = camera
+    // const post = posts[0]
     const height = Dimensions.get("window").height
-    var video_source, image_source
-    if (!!post && !!post.video) { 
-      video_source = getAsset(post.video.high.url_name) || {uri: post.video.high.url}
-      image_source = getAsset(post.video.high.poster_name)  || post.video.high.poster
-    } 
     return (
       <React.Fragment>
-        { Platform.OS != 'ios' && !showHeader ? <StatusBar translucent backgroundColor="transparent" /> : null }
         <ScrollView onScroll={this.handleScroll}>
-          { !!post && !!post.picture.url && (
-            <Image 
-              source={{uri: post.picture.mobile.url }} 
-              style={{height: height}} 
-            /> 
-          )
-          }
-          { !!post && !!post.video && (
-            <Video
-              source={video_source}
-              poster={image_source}
-              posterResizeMode="cover"
-              resizeMode="cover"
-              style={{height: height}}
-              repeat 
-              muted 
-            />
-            )
-          } 
-          { !!post && <ForecastHourly location={location} forecast_info={forecast_info} /> }
-          <ForecastMap location={location} />
+          {/* !!post && <ForecastHourly location={location_attributes} forecast_info={forecast_info} /> */}
+          <ForecastMap 
+            location={location_attributes} 
+            cameras={cameras} />
           <TableView daily={daily} />
         </ScrollView>
       </React.Fragment>
