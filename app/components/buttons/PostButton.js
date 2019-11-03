a911ea5b2be940af7a6174b7a3a0baf2abb47d94
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-11-03 12:06:14 +0800

    post button component to render dates

---
+++ app/components/buttons/PostButton.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+export default function PostButton({ data, action, index, selected }) {
+  const is_selected = index == selected
+  const color = is_selected ? styles.highlight : styles.normal
+  console.warn(data.created_at);
+  return (
+    <React.Fragment>
+      <TouchableOpacity 
+        style={[
+          styles.button,
+          color
+        ]}
+        onPress={() => action(index)}
+      >
+        <Text
+          style={styles.text}>
+          { data.created_at }
+        </Text>
+      </TouchableOpacity>
+    </React.Fragment>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    // height: 50,
+    // width: 70,
+    // borderRadius: 10,
+    // display: 'flex',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // zIndex: 2,
+  }, 
+  highlight: {
+    // borderColor: '#e6e6e6',
+    // backgroundColor: '#66ff66',
+    // backgroundColor: '#66ff66',
+    // borderWidth: 1,
+  },
+  normal: {
+    // borderColor: 'white',
+    // borderWidth: 2,
+  },
+  text: {
+    // color: 'white',
+    // fontWeight: '900',
+    // textShadowColor: 'rgba(0, 0, 0, 1)',
+    // textShadowOffset: {width: -1, height: 1},
+    // textShadowRadius: 1,
+    // zIndex:2,
+  }
+})
