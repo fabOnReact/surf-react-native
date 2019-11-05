ad260761fc6a26b00fe0887f1c90307bcec77a18
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-11-05 11:23:50 +0800

    minor changes

--- app/lib/support.js
+++ app/lib/support.js
@@ -60,28 +60,28 @@ export function isPresent(obj) {
 }
 
 // image_source = Image.resolveAssetSource(getAsset(post.video.high.poster_name)).uri  // || post.video.high.poster
-// export const getAsset = (key) => {
-//   const assetsArray = {
-//     "surfer-max.mp4": require('../videos/surfer-max.mp4'),
-//     "surfer-min.mp4": require('../videos/surfer-min.mp4'),
-//     "seaside-max.mp4": require('../videos/seaside-max.mp4'),
-//     "seaside-min.mp4": require('../videos/seaside-min.mp4'),
-//     "seaside-poster.png": Image.resolveAssetSource(require('../images/seaside-poster.png')).uri,
-//     "seaside-poster-max.png": Image.resolveAssetSource(require('../images/seaside-poster-max.png')).uri,
-//     "bronte-max.mp4": require('../videos/bronte-max.mp4'),
-//     "bronte-min.mp4": require('../videos/bronte-min.mp4'),
-//     "bronte-poster.png": Image.resolveAssetSource(require('../images/bronte-poster.png')).uri,
-//     "bronte-poster-max.png": Image.resolveAssetSource(require('../images/bronte-poster-max.png')).uri,
-//     "costline-max.mp4": require('../videos/costline-max.mp4'),
-//     "costline-min.mp4": require('../videos/costline-min.mp4'),
-//     "costline-poster.png": Image.resolveAssetSource(require('../images/costline-poster.png')).uri,
-//     "costline-poster-max.png": Image.resolveAssetSource(require('../images/costline-poster-max.png')).uri,
-//     "surfer-poster-max.png": Image.resolveAssetSource(require('../images/surfer-poster-max.png')).uri,
-//     "surfer-poster.png": Image.resolveAssetSource(require('../images/surfer-poster.png')).uri,
-//   }
-//   return assetsArray[key];
-// };
-//
+export const getAsset = (key) => {
+  const assetsArray = {
+    "surfer-max.mp4": require('../videos/surfer-max.mp4'),
+    "surfer-min.mp4": require('../videos/surfer-min.mp4'),
+    "seaside-max.mp4": require('../videos/seaside-max.mp4'),
+    "seaside-min.mp4": require('../videos/seaside-min.mp4'),
+    "seaside-poster.png": Image.resolveAssetSource(require('../images/seaside-poster.png')).uri,
+    "seaside-poster-max.png": Image.resolveAssetSource(require('../images/seaside-poster-max.png')).uri,
+    "bronte-max.mp4": require('../videos/bronte-max.mp4'),
+    "bronte-min.mp4": require('../videos/bronte-min.mp4'),
+    "bronte-poster.png": Image.resolveAssetSource(require('../images/bronte-poster.png')).uri,
+    "bronte-poster-max.png": Image.resolveAssetSource(require('../images/bronte-poster-max.png')).uri,
+    "costline-max.mp4": require('../videos/costline-max.mp4'),
+    "costline-min.mp4": require('../videos/costline-min.mp4'),
+    "costline-poster.png": Image.resolveAssetSource(require('../images/costline-poster.png')).uri,
+    "costline-poster-max.png": Image.resolveAssetSource(require('../images/costline-poster-max.png')).uri,
+    "surfer-poster-max.png": Image.resolveAssetSource(require('../images/surfer-poster-max.png')).uri,
+    "surfer-poster.png": Image.resolveAssetSource(require('../images/surfer-poster.png')).uri,
+  }
+  return assetsArray[key];
+};
+
 // import Dimensions from 'Dimensions';
 // import { Header } from 'react-navigation';
 // class Element {
