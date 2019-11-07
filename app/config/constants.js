6b7ef2ef18ebca1e8bf3b21ad42393ce7060db68
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-11-07 10:00:08 +0800

    update ip

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,9 +1,9 @@
 // export const host = 'https://surfcheck.xyz/api/v1'
-export const host = 'http://192.168.0.201:3000/api/v1'
+// export const host = 'http://192.168.0.201:3000/api/v1'
 // starbucks beach
 // export const host = 'http://10.114.67.188:3000/api/v1'
 // export const host = 'http://192.168.0.102:3000/api/v1'
 // export const host = 'http://192.168.43.59:3000/api/v1'
-// export const host = 'http://192.168.1.127:3000/api/v1'
+export const host = 'http://192.168.1.127:3000/api/v1'
 // export const host = 'http://169.254.162.157:3000/api/v1'
 // export const host = "https://d502b9f2-3144-4162-b6d6-1ce798aafc39.mock.pstmn.io/api/v1"
