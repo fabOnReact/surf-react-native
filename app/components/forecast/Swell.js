6d1d0ce00a5456d20ac62e82cf5682a309c4c135
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-11-08 08:21:33 +0800

    converting units to ft

--- app/components/forecast/Swell.js
+++ app/components/forecast/Swell.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text } from 'react-native';
+import Unit from '../../lib/unit';
 
 export default class Swell extends Component {
   render() {
     const { swellHeight, period, styles} = this.props
-    const forecast = `${swellHeight} mt. @ ${parseInt(period)} s.`
+    const swell = new Unit({ european: false, unit: "ft.", value: swellHeight })
+    const forecast = `${swell.value} ${swell.unit} @ ${parseInt(period)} s.`
     return (
       <Text style={styles}>
         { forecast }
