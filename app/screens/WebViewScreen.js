607ad60add01f0414a66862cae5ef550a238da90
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-11-12 09:41:47 +0800

    adding header to WebView

--- app/screens/WebViewScreen.js
+++ app/screens/WebViewScreen.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-import { WebView } from 'react-native-webview';
+import { StatusBar, WebView } from 'react-native-webview';
 import { host } from '../config/constants';
 
 export default class WebViewScreen extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
       title: navigation.getParam('title', ''),
-    };
-  };
+      headerTintColor: 'black' ,
+      headerStyle: { marginTop: StatusBar.currentHeight },
+    }
+  }
 
   render() {
     const { navigation } = this.props
     const path = navigation.getParam('path', '')
     const url = 'https://surfcheck.xyz' + path
     return (
-      <WebView source={{uri: url}} />
+      <React.Fragment>
+        <StatusBar 
+          backgroundColor="black" 
+          hidden={false} 
+          barStyle="dark-content" 
+          translucent={false} />
+        <WebView source={{uri: url}} />
+      </React.Fragment>
     );
   }
 }
