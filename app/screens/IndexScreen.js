d986c40b6baa6851a7466eab8bbbe1bb36ec9ea6
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-11-12 08:45:03 +0800

    adding video background while loading for main page

--- app/screens/IndexScreen.js
+++ app/screens/IndexScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { BackHandler, DeviceEventEmitter, Alert, View, Text, StatusBar, Platform, StyleSheet } from 'react-native';
+import { Dimensions, BackHandler, DeviceEventEmitter, Alert, View, Text, StatusBar, Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import Video from 'react-native-video';
 import AsyncStorage from '@react-native-community/async-storage';
 import Spinner from 'react-native-loading-spinner-overlay';
 import Locations from '../components/index/Locations';
 import LocationPermission from '../components/LocationPermission';
+import { getAsset } from '../lib/support'
 
 export default class IndexScreen extends Component {
   state = { spinner: true, credentials: null };
@@ -18,6 +20,10 @@ export default class IndexScreen extends Component {
     this.setState({ spinner: false }) 
   }
 
+  pageLoading = () => {
+    this.setState({ spinner: true })
+  }
+
   setCredentials = async () => {
     const email = await AsyncStorage.getItem('userEmail')
     const token = await AsyncStorage.getItem('userToken')
@@ -29,25 +35,38 @@ export default class IndexScreen extends Component {
   }
 
   render() {
-    const { credentials } = this.state
+    const { credentials, spinner } = this.state
     const credentials_found = !!credentials
     const ios = Platform.OS === 'ios'
+    const height = Dimensions.get("window").height;
     return (
       <React.Fragment>
         { ios ? <LocationPermission /> : null } 
         <StatusBar translucent backgroundColor="transparent" barStyle="light-content" />
+        {
+          spinner && <Video 
+            source={getAsset("costline-max.mp4")}
+            poster={getAsset("costline-poster-max.png")}
+            posterResizeMode={"cover"}
+            resizeMode={"cover"}
+            style={{height: height}}
+            repeat 
+            muted
+          />
+        }
         <Spinner
-          visible={this.state.spinner}
+          visible={spinner}
           textContent={'Loading...'}
           textStyle={styles.spinnerTextStyle}
-          color="black"
-        />
+          color="white"
+        /> 
         { 
           credentials_found && <Locations 
             navigation={this.props.navigation} 
             loaded={this.pageIsLoaded} 
             locationAlert={this._alertForLocationPermission} 
             credentials={credentials}
+            loading={this.pageLoading}
           /> 
         }
       </React.Fragment>
