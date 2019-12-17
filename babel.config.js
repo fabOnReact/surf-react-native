a76d5af9b7491961c23b064b95b4299f75d3e10e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-12-17 16:05:47 +0100

    babel config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
 
-  // const presets =  ["react-native", "module:react-native-dotenv"];
   const presets = ["module:metro-react-native-babel-preset", "module:react-native-dotenv"]
   const plugins = ["@babel/plugin-transform-flow-strip-types", "@babel/plugin-proposal-class-properties", "@babel/plugin-proposal-nullish-coalescing-operator"];
 
