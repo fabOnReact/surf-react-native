a4fd7eabb63f9640c80445da08cfae2e6abb9554
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-12-20 19:07:11 +0100

    first spec e2e

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -3,17 +3,21 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it('finds the Email input', async () => {
+    await expect(element(by.text('Email'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  it('shows error message with invalid credentials', async () => {
+    await element(by.text('Sign In')).tap();
+    await expect(element(by.id("errorMessage"))).toBeVisible();
   });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it.only('authenticates the test user', async () => {
+    await element(by.type('RCTUITextField').and(by.text('email'))).typeText('testing@torino1')
+    // <UIAccessibilityTextFieldElement:0x6000027355c0; AX=Y; AX.id='RNE__Input__text-input'; AX.value='Email'; AX.frame={{10, 111}, {394, 40}}; AX.acti
+    // vationPoint={207, 131}; AX.traits='UIAccessibilityTraitNone'; AX.focused='N'>
+    // await element(by.text('Password')).replaceText('fabrizio')
+    // await element(by.text('Sign In')).tap();
+    // await expect(element(by.text('World!!!'))).toBeVisible();
   });
 });
