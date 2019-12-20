21e2419471c57e5d578fcc69304a2484ddf30028
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-12-20 19:06:49 +0100

    e2e init config

--- e2e/init.js
+++ e2e/init.js
@@ -12,7 +12,8 @@ jasmine.getEnv().addReporter(adapter);
 jasmine.getEnv().addReporter(specReporter);
 
 beforeAll(async () => {
-  await detox.init(config);
+  await detox.init(config, { launchApp: false });
+  await device.launchApp({newInstance: true, permissions: {location: 'inuse'}});
 });
 
 beforeEach(async () => {
