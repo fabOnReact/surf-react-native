734b871ce263715aaa538e344b6abc0e561f1fff
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-12-27 12:04:38 +0100

    removing commented code

--- e2e/__mocks__/react-native-permissions.js
+++ e2e/__mocks__/react-native-permissions.js
@@ -2,11 +2,6 @@ import * as RNPermission from 'react-native-permissions';
 const { PERMISSIONS, RESULTS } = require('react-native-permissions/lib/commonjs/constants.js');
 
 export { PERMISSIONS, RESULTS };
-// mock out any functions you want in this style...
 export async function check(permission) {
   jest.fn();
 }
-// const {
-//   PERMISSIONS,
-//   RESULTS,
-// } = require('react-native-permissions/lib/commonjs/constants.js');
