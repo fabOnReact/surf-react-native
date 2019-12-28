52ee28943f9ae68e8bbed6fa7ec5aaf2b715aee8
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-12-28 17:58:55 +0100

    unlink all dependencies

--- android/app/src/main/java/com/surfapp/MainApplication.java
+++ android/app/src/main/java/com/surfapp/MainApplication.java
@@ -3,20 +3,7 @@ package com.surfapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.thebylito.navigationbarcolor.NavigationBarColorPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.swmansion.rnscreens.RNScreensPackage;
-import com.bugsnag.BugsnagReactNative;
-import com.brentvatne.react.ReactVideoPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.horcrux.svg.SvgPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.airbnb.android.react.maps.MapsPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import org.wonday.orientation.OrientationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -38,18 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new NavigationBarColorPackage(),
-            new RNDeviceInfo(),
-            new RNScreensPackage(),
-            BugsnagReactNative.getPackage(),
-            new ReactVideoPackage(),
-            new RNCameraPackage(),
-            new SvgPackage(),
-            new AsyncStoragePackage(),
-            new LocationServicesDialogBoxPackage(),
-            new RNCWebViewPackage(),
-            new MapsPackage(),
             new RNGoogleSigninPackage(),
              new RNFusedLocationPackage()
       );
