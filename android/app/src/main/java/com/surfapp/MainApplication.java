6a12298c6d7e1a937f0d93e2541964fdb90d714b
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-12-28 17:50:11 +0100

    react-native upgrade 0.60.6 ANDROID

--- android/app/src/main/java/com/surfapp/MainApplication.java
+++ android/app/src/main/java/com/surfapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.surfapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.thebylito.navigationbarcolor.NavigationBarColorPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.swmansion.rnscreens.RNScreensPackage;
@@ -37,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new NavigationBarColorPackage(),
             new RNDeviceInfo(),
             new RNScreensPackage(),
