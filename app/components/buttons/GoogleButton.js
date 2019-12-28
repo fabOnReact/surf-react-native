938f6ba3c6bc2ecde0c7b00ff69a702cd5ed7638
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-12-28 17:41:22 +0100

    fixing error with missing vector icons

--- app/components/buttons/GoogleButton.js
+++ app/components/buttons/GoogleButton.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+import { GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
 import { configureGoogleSignIn, getGoogleUser, createResource } from '../../lib/api';
 import { errorMessage, userSettings } from '../../lib/support';
 
