836689f947faa78674afb35270080eb1d77db969
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-02 11:34:16 +0100

    fixing RCTRootView cancelTouches is deprecated
    https://github.com/software-mansion/react-native-gesture-handler/issues/746

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import { AppRegistry } from 'react-native'
+import { YellowBox, AppRegistry } from 'react-native'
 import App from './app/App';
 import {name as appName} from './app.json';
 
 import { Client, Configuration } from 'bugsnag-react-native';
 import { BUGSNAG_ID } from 'react-native-dotenv';
+import 'react-native-gesture-handler';
+
+YellowBox.ignoreWarnings([
+  'VirtualizedLists should never be nested',
+  'RCTRootView cancelTouches',
+]);
 
 const config = new Configuration(BUGSNAG_ID);
 config.appVersion = require('./package.json').version;
