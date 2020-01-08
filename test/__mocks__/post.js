2062a606171c069b1d0cdccf43c46221f533cd27
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-08 18:44:04 +0100

    post mock

---
+++ test/__mocks__/post.js
@@ -0,0 +1,14 @@
+export const post = { 
+  forecast: 
+    { 
+      "windSpeed": 3.5, 
+      "waveHeight": 1, 
+      "swellHeight": 1, 
+      "swellPeriod": 13, 
+      "optimal_wind": true, 
+      "optimal_swell": true, 
+      "waveDirection": 204, 
+      "windDirection": 221, 
+      "swellDirection": 204
+    }
+}
