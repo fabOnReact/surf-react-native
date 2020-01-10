7e29c9abcc29101e090339829f6d6645fcac42e8
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-01-10 11:49:47 +0100

    update ip

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,2 +1,2 @@
-// export const host = 'https://surfcheck.xyz/api/v1'
-export const host = "http://192.168.1.151:3000/api/v1"
+export const host = 'https://surfcheck.xyz/api/v1' 
+export const host = 'http://192.168.1.158:3000'
